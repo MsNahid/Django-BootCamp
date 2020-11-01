4343a1dc5c4461eff15d2c512c29ed10234c54e2
MsNahid <mssnahid729@gmail.com>
2020-11-01 21:06:29 +0600

    errors and exception

---
+++ python level 2/Errors and Exception.py
@@ -0,0 +1,26 @@
+# try:
+
+#     # f = open("simple.txt", "w")
+#     f = open("simple.txt", "r")
+#     f.write("Test write to simple text!")
+
+# except IOError: #use only except for all kind of errorx
+#     print("ERROR: COULD NOT FIND FILE OR READ DATA!")
+# else:
+#     print("SUCCESS!")
+#     f.close()
+
+# print("Hello World!!")
+try:
+
+    # f = open("simple.txt", "w")
+    f = open("simple.txt", "r")
+    f.write("Test write to simple text!")
+
+except IOError: #use only except for all kind of errorx
+    print("ERROR: COULD NOT FIND FILE OR READ DATA!")
+finally:
+    print("I always work, no matter what is happing or happened!")
+    f.close()
+
+print("Hello World!!")
---
+++ python level 2/scope.py
@@ -0,0 +1,6 @@
+x = 25
+
+def  my_func():
+    x = 50
+
+print(x)
