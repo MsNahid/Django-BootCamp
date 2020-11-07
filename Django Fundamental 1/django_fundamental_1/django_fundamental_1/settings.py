e1cb0baab428bc3829637c5cfd042e77e9401e7f
MsNahid <mssnahid729@gmail.com>
2020-11-07 10:50:33 +0600

    using templates for cross platfrom

--- Django Fundamental 1/django_fundamental_1/django_fundamental_1/settings.py
+++ Django Fundamental 1/django_fundamental_1/django_fundamental_1/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# print(__file__)
+# print(os.path.abspath(__file__))
+# print(BASE_DIR)
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'django_fundamental_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
